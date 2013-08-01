fd53b35da02aec431c08cb1f19cf2a0539efe482
will <will@willhaley.com>
2013-08-01 08:43:05 -0500

    About menu item and activity.  Ability to search for Radar Site.

--- MobileDoppler/build/source/r/debug/com/willhaley/android/mobiledoppler/R.java
+++ MobileDoppler/build/source/r/debug/com/willhaley/android/mobiledoppler/R.java
@@ -21,22 +21,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int cityImageView=0x7f090006;
-        public static final int countyImageView=0x7f090004;
-        public static final int horizontalScrollView=0x7f090002;
+        public static final int action_settings=0x7f09000a;
+        public static final int cityImageView=0x7f090007;
+        public static final int countyImageView=0x7f090005;
+        public static final int editText=0x7f090001;
+        public static final int horizontalScrollView=0x7f090003;
         public static final int listView=0x7f090000;
-        public static final int scrollView=0x7f090001;
-        public static final int subTitle=0x7f090008;
-        public static final int title=0x7f090007;
-        public static final int topographyImageView=0x7f090003;
-        public static final int weatherImageView=0x7f090005;
+        public static final int scrollView=0x7f090002;
+        public static final int subTitle=0x7f090009;
+        public static final int title=0x7f090008;
+        public static final int topographyImageView=0x7f090004;
+        public static final int weatherImageView=0x7f090006;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_radar_site=0x7f030002;
-        public static final int radar_site_layout=0x7f030003;
+        public static final int radar_site_list_item=0x7f030003;
     }
     public static final class menu {
         public static final int about=0x7f080000;
--- MobileDoppler/src/main/java/com/willhaley/android/mobiledoppler/controller/MainActivity.java
+++ MobileDoppler/src/main/java/com/willhaley/android/mobiledoppler/controller/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Filter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,11 +29,13 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
-public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, TextWatcher {
 
     private Context context;
     private RadarSite[] radarSites;
+    private RadarSitesAdapter radarSitesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +46,25 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
         loadRadarSites();
 
-        RadarSitesAdapter radarSitesAdapter = new RadarSitesAdapter(radarSites);
+        radarSitesAdapter = new RadarSitesAdapter(radarSites);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(radarSitesAdapter);
         listView.setOnItemClickListener(this);
+        listView.setTextFilterEnabled(true);
+
+        ((EditText) findViewById(R.id.editText)).addTextChangedListener(this);
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) { }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+        radarSitesAdapter.getFilter().filter(charSequence.toString());
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) { }
 
     private void loadRadarSites() {
         InputStream inputStream = getResources().openRawResource(R.raw.radar_sites);
@@ -98,24 +118,33 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int index, long id) {
-        RadarSite radarSite = radarSites[index];
+        RadarSite radarSite = (RadarSite) radarSitesAdapter.getItem(index);
         Intent intent = new Intent(context, RadarSiteActivity.class);
         intent.putExtra("RadarSite", radarSite);
         startActivity(intent);
     }
 
     class RadarSitesAdapter extends ArrayAdapter {
-        private static final int LAYOUT_ID = R.layout.radar_site_layout;
+        private static final int LAYOUT_ID = R.layout.radar_site_list_item;
         private RadarSite[] radarSites;
+        private RadarSite[] filteredRadarSites;
+        private RadarSiteFilter filter;
 
         public RadarSitesAdapter(RadarSite[] radarSites) {
             super(context, LAYOUT_ID, radarSites);
             this.radarSites = radarSites;
+            this.filteredRadarSites = this.radarSites.clone();
+            this.filter = new RadarSiteFilter();
         }
 
         @Override
         public int getCount() {
-            return radarSites.length;
+            return filteredRadarSites.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return this.filteredRadarSites[position];
         }
 
         @Override
@@ -125,7 +154,9 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
                 LayoutInflater inflater = getLayoutInflater();
                 rowView = inflater.inflate(LAYOUT_ID, null);
             }
-            RadarSite radarSite = radarSites[position];
+
+            RadarSite radarSite = (RadarSite) getItem(position);
+
             if(radarSite.area != null) {
                 ((TextView) rowView.findViewById(R.id.title)).setText(radarSite.area);
             }
@@ -134,9 +165,49 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
             }
             return rowView;
         }
+
+        @Override
+        public Filter getFilter() {
+            return this.filter;
+        }
+
+        class RadarSiteFilter extends Filter {
+            // http://stackoverflow.com/questions/2718202/custom-filtering-in-android-using-arrayadapter
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                // NOTE: this function is *always* called from a background thread, and
+                // not the UI thread.
+                constraint = constraint.toString().toLowerCase();
+                FilterResults result = new FilterResults();
+                if(constraint != null && constraint.toString().length() > 0) {
+                    ArrayList<RadarSite> filteredSites = new ArrayList<RadarSite>();
+                    for(RadarSite radarSite : radarSites) {
+                        if(radarSite.siteId.toLowerCase().contains(constraint)) {
+                            filteredSites.add(radarSite);
+                        } else if(radarSite.area.toLowerCase().contains(constraint)) {
+                            filteredSites.add(radarSite);
+                        }
+                    }
+                    result.count = filteredSites.size();
+                    result.values = filteredSites.toArray();
+                } else {
+                    result.count = radarSites.length;
+                    result.values = radarSites;
+                }
+                return result;
+            }
+
+            // NOTE: this function is *always* called from the UI thread.
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                Object[] values = (Object[]) filterResults.values;
+                filteredRadarSites = new RadarSite[values.length];
+                System.arraycopy(values, 0, filteredRadarSites, 0, values.length);
+                radarSitesAdapter.notifyDataSetInvalidated();
+            }
+        }
     }
 
-    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.about, menu);
@@ -149,5 +220,4 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         startActivity(intent);
         return super.onMenuItemSelected(featureId, item);
     }
-    */
 }
