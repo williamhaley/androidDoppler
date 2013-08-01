075d830e8110ccbfe76158d386397033c0abd3a4
will <will@willhaley.com>
2013-07-31 23:27:14 -0500

    initial commit

---
+++ MobileDoppler/src/main/java/com/willhaley/android/mobiledoppler/model/RadarSite.java
@@ -0,0 +1,11 @@
+package com.willhaley.android.mobiledoppler.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by will on 7/31/13.
+ */
+public class RadarSite implements Serializable {
+    public String siteId;
+    public String area;
+}
